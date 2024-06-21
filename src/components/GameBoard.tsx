97de84aa4050bf096c1e1d27dc9d125c19e0bb65
Saswat Das <saswatd.official@gmail.com>
2024-06-22 00:00:08 +0530

    chore: change onClick to onMouseDown for better ux

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -21,7 +21,7 @@ export default function GameBoard({
           className={`w-24 h-24 text-4xl font-bold rounded-lg transition-all duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-400 ${
             cell ? "bg-gray-700" : "bg-gray-800 hover:bg-gray-700"
           }`}
-          onClick={() => handleMove(index)}
+          onMouseDown={() => handleMove(index)}
           disabled={!!cell || !!winner || currentPlayer === "O"}
         >
           {cell}
